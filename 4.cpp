0c44bcedb6613420d62936fd29314b123b66fb95
atiwari3bu <atiwari3@binghamton.edu>
2018-09-21 15:23:22 -0400

    Palindrome Number

---
+++ 4.cpp
@@ -0,0 +1,35 @@
+#include<stdlib.h>
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string org=to_string(x);
+        string rev;
+        for(auto iter=org.end()-1;iter>=org.begin();--iter){
+            rev.push_back(*iter);
+        }
+        if(org==rev) return true;
+        else return false;
+    }
+};
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+string boolToString(bool input) {
+    return input ? "True" : "False";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        int x = stringToInteger(line);
+        
+        bool ret = Solution().isPalindrome(x);
+
+        string out = boolToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
