5b1e20bef2f5f8788525930ece48c2e680ecafac
atiwari3bu <atiwari3@binghamton.edu>
2018-09-22 13:18:56 -0400

    Longest Common Prefix

---
+++ 6.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size()==(size_t)0) return "";
+        auto iter=strs.begin();
+        string first=*iter;
+        string temp;
+        int j;
+        
+        for(int i=0;i!=first.length();++i){
+            for(auto iter=strs.begin()+1;iter!=strs.end();++iter){
+                string str=*iter;
+                if(first[i]==str[i]) {
+                    ++j; 
+                }
+            }
+            
+            if(j==((int)(strs.end()-strs.begin()-1))){
+                temp.push_back(first[i]);
+                j=0;
+            }   
+            else break;
+        }
+        //cout<<temp<<endl;
+        return temp;
+    }
+};
+
