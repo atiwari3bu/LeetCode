472c74b96af3dfebd8991d50dabba7c5b4c66700
Ashish Tiwari <atiwari3@cs.binghamton.edu>
2018-09-25 20:10:16 -0400

    Valid Parenthesis

--- 7.cpp
+++ 7.cpp
@@ -1,78 +1,30 @@
+#include<string>
+
 class Solution {
 public:
-    
-    void showStack(std::stack<char> st){  // For debugging
-        while(!st.empty()){
-            cout<<st.top()<<endl;
-            st.pop();
+    int reverse(int x) {
+        long int num=0;
+        while(x!=0){
+            num=num*10+x%10;
+            if(num>INT_MAX || num<INT_MIN) return 0;
+            x=x/10;
         }
-    }
-    
-    bool isValid(string s) {
-        std::stack<char> st;
-        if(*s.begin()==')' || *s.begin()==']' || *s.begin()=='}') return false;
-        for(auto iter=s.begin();iter!=s.end();++iter){
-            if(*iter=='(' || *iter=='{' || *iter=='[') st.push(*iter);
-            else{
-                if(st.size()==0) return false;
-                else if(st.top()=='(' && *iter==')'){
-                    st.pop();
-                    continue;
-                }
-                else if(st.top()=='[' && *iter==']'){
-                    st.pop();
-                     continue;
-                }
-                else if(st.top()=='{' && *iter=='}'){ 
-                    st.pop();
-                    continue;
-                }
-                else return false;
-            }
-        }
-        if(st.size()!=0) return false;
-        return true;
+        return (int)num;
     }
 };
 
-string stringToString(string input) {
-    assert(input.length() >= 2);
-    string result;
-    for (int i = 1; i < input.length() -1; i++) {
-        char currentChar = input[i];
-        if (input[i] == '\\') {
-            char nextChar = input[i+1];
-            switch (nextChar) {
-                case '\"': result.push_back('\"'); break;
-                case '/' : result.push_back('/'); break;
-                case '\\': result.push_back('\\'); break;
-                case 'b' : result.push_back('\b'); break;
-                case 'f' : result.push_back('\f'); break;
-                case 'r' : result.push_back('\r'); break;
-                case 'n' : result.push_back('\n'); break;
-                case 't' : result.push_back('\t'); break;
-                default: break;
-            }
-            i++;
-        } else {
-          result.push_back(currentChar);
-        }
-    }
-    return result;
-}
-
-string boolToString(bool input) {
-    return input ? "True" : "False";
+int stringToInteger(string input) {
+    return stoi(input);
 }
 
 int main() {
     string line;
     while (getline(cin, line)) {
-        string s = stringToString(line);
+        int x = stringToInteger(line);
         
-        bool ret = Solution().isValid(s);
+        int ret = Solution().reverse(x);
 
-        string out = boolToString(ret);
+        string out = to_string(ret);
         cout << out << endl;
     }
     return 0;
