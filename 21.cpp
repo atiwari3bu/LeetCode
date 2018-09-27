6f856f90a9bfb4cb75c68b4794b6fe183800241e
Ashish Tiwari <atiwari3@cs.binghamton.edu>
2018-09-27 18:59:02 -0400

    Merge Two Sorted Lists

---
+++ 21.cpp
@@ -0,0 +1,114 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* l=new ListNode(0);
+        ListNode* top=l;
+        
+        if(l1==nullptr && l2==nullptr){ return l1;}
+        
+        while(l1!=nullptr && l2!=nullptr){
+            if(l1->val<=l2->val){
+                l->next=new ListNode(l1->val);
+                l=l->next;
+                l1=l1->next;
+            }
+            else{
+                l->next=new ListNode(l2->val);
+                l=l->next;
+                l2=l2->next;
+            }
+        }
+        
+        while(l1!=nullptr){
+            l->next=new ListNode(l1->val);
+            l=l->next;
+            l1=l1->next;
+        }
+        
+        while(l2!=nullptr){
+            l->next=new ListNode(l2->val);
+            l=l->next;
+            l2=l2->next;
+        }
+        return top->next;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+ListNode* stringToListNode(string input) {
+    // Generate list from the input
+    vector<int> list = stringToIntegerVector(input);
+
+    // Now convert that list into linked list
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    for(int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+string listNodeToString(ListNode* node) {
+    if (node == nullptr) {
+        return "[]";
+    }
+
+    string result;
+    while (node) {
+        result += to_string(node->val) + ", ";
+        node = node->next;
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        ListNode* l1 = stringToListNode(line);
+        getline(cin, line);
+        ListNode* l2 = stringToListNode(line);
+        
+        ListNode* ret = Solution().mergeTwoLists(l1, l2);
+
+        string out = listNodeToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
