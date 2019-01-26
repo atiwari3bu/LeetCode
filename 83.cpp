0974282d8daf38b9dc0279993086c7321dffcd77
atiwari3bu <atiwari3@binghamton.edu>
2019-01-26 09:39:55 -0500

    Remove Duplicates from Sorted List

---
+++ 83.cpp
@@ -0,0 +1,106 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void removeNode(ListNode* head,ListNode* remove){
+        ListNode* traverse=head;
+        while(traverse->next!=remove){
+            traverse=traverse->next;
+        }
+        traverse->next=traverse->next->next;
+    }
+    
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* traverse=head;
+        if(head==nullptr) return head;
+        int dup=head->val;
+        while(traverse!=nullptr){
+            //std::cout<<traverse->val<<std::endl;
+            if(dup==traverse->val && traverse!=head){
+                //std::cout<<"Duplicate is "<<traverse->val<<std::endl;
+                removeNode(head,traverse);
+                traverse=traverse->next;
+                continue;
+            }
+            dup=traverse->val;
+            traverse=traverse->next;
+        }
+        return head;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+ListNode* stringToListNode(string input) {
+    // Generate list from the input
+    vector<int> list = stringToIntegerVector(input);
+
+    // Now convert that list into linked list
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    for(int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+string listNodeToString(ListNode* node) {
+    if (node == nullptr) {
+        return "[]";
+    }
+
+    string result;
+    while (node) {
+        result += to_string(node->val) + ", ";
+        node = node->next;
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        line="[1,1,2,3,3]";
+        ListNode* head = stringToListNode(line);
+        
+        ListNode* ret = Solution().deleteDuplicates(head);
+
+        string out = listNodeToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
