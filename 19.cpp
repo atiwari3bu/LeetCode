fbba3e6e45f2c807b5bbcfa8fda5dd93e7543cc4
atiwari3bu <atiwari3@binghamton.edu>
2019-02-08 15:03:23 -0500

    Remove Nth Node From End of List

---
+++ 19.cpp
@@ -0,0 +1,113 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+    int calLength(ListNode* head){
+        int length=0;
+        while(head!=nullptr){
+            ++length;
+            head=head->next;
+        }
+        return length;
+    }
+    
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int length=calLength(head);
+        int pos=length-n;
+        ListNode* traverse=head;
+        std::cout<<"Length is "<<length<<" Postion is "<<pos<<std::endl;
+        
+        int i=1;
+        while(i<pos){
+            traverse=traverse->next;
+            ++i;
+        }
+        if(pos==0 && length>1) head=traverse->next;
+        if(traverse->next==nullptr && length==1) return nullptr; // for 1 element
+        traverse->next=traverse->next->next;
+        return head;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+ListNode* stringToListNode(string input) {
+    // Generate list from the input
+    vector<int> list = stringToIntegerVector(input);
+
+    // Now convert that list into linked list
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    for(int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+string listNodeToString(ListNode* node) {
+    if (node == nullptr) {
+        return "[]";
+    }
+
+    string result;
+    while (node) {
+        result += to_string(node->val) + ", ";
+        node = node->next;
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        line="[1,2]";
+        ListNode* head = stringToListNode(line);
+        getline(cin, line);
+        int n = stringToInteger(line);
+        n=1;
+        
+        ListNode* ret = Solution().removeNthFromEnd(head, n);
+
+        string out = listNodeToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
